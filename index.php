401fdc5a4a2d5366bef2e6795a100ec141af26e2
ashabilpunya <ashabilrizqi@gmail.com>
2025-06-03 13:43:48 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
 
   <h1>hello world</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptates ea temporibus, commodi blanditiis
+    laborum aspernatur libero rem eveniet id maiores corporis earum molestias obcaecati? Officiis temporibus incidunt
+    laboriosam praesentium!
+  </p>
 
 </body>
 
